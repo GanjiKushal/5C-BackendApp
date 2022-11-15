ba071eb666323544b70270cbc7e4814ed9dcd14b
GanjiKushal <kushalganjiganji@gmail.com>
2022-11-15 19:21:07 +0530

    getroute is working properly and added to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const mongoose=require('mongoose')
 const app=express()
 const port=8080
-// const route1=require('./routes/getroute')
+const route1=require('./routes/getroute')
 // const route2=require('./routes/postroute')
 // const route3=require('./routes/put&deleteroute')
 //connecting to database
@@ -16,7 +16,7 @@ mongoose.connect('mongodb+srv://kushal:Kushal24@cluster0.q2aawhd.mongodb.net/tes
 })
 
 app.use(express.json())
-// app.use("/",route1)
+app.use("/",route1)
 // app.use("/",route2)
 // app.use("/",route3)
 
